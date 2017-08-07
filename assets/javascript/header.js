9f9a569290d86e2cbc9c88576f02c1496fa89e48
Anthony <an538811@gmail.com>
2017-08-07 12:02:43 -0500

    input bar functional

--- assets/javascript/header.js
+++ assets/javascript/header.js
@@ -1,13 +1,13 @@
-function searchIconClicked()
+$(".submitButton").on("click", function()
 {
 	var input = $(".form-control").val().trim();
 	searchInput(input);
-}
+});
 //sets the search field into session storage
 function searchInput(characterInput) 
 {
 	console.log(characterInput);
-    sessionStorage.setItem("userInput", input);
+    sessionStorage.setItem("userInput", characterInput);
  if (sessionStorage.getItem("userInput") != "")
  	{
   		window.location.href = "index2.html";
